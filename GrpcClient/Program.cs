eae2e9df5c954265d47012124fc62a1f0881aed9
Lazar Dragos Iulian <lazar.dragos.iulian@gmail.com>
2020-04-27 13:23:03 +0300

    tema2

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,6 +2,8 @@
 using Grpc.Net.Client;
 using GrpcServer;
 using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -9,20 +11,31 @@ namespace GrpcClient
 {
     class Program
     {
+
+
+
         static async Task Main(string[] args)
         {
-            
 
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var customerClient = new Customer.CustomerClient(channel);
             var input = Console.ReadLine();
-            var clientRequested = new CustomerLookupModel { UserId = input.ToString() };
+            DateTime dDate;
 
-            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+            if (DateTime.TryParse(input, out dDate))
+            {
+                String.Format("{0:d/MM/yyyy}", dDate);
+               var clientRequested = new CustomerLookupModel { UserId = input.ToString() };
 
-            Console.WriteLine($"{ customer.FirstName } { customer.LastName }");
+                var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
 
-          
+                Console.WriteLine($"{ customer.FirstName } { customer.LastName }");
+            }
+            else
+            {
+                Console.WriteLine("Eroare"); 
+            }
+
             Console.ReadLine();
         }
     }
--- GrpcServer/Services/CustomersService.cs
+++ GrpcServer/Services/CustomersService.cs
@@ -16,10 +16,89 @@ namespace GrpcServer.Services
             _logger = logger;
         }
 
+        
+
         public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
         {
+            var data = request.UserId;
             CustomerModel output = new CustomerModel();
-            Console.WriteLine("Hello," + request.UserId);
+            Console.WriteLine("data: " + data);
+            DateTime dt;
+            if (DateTime.TryParse(data, out dt))
+            {
+                String.Format("{0:d/MM/yyyy}", dt);
+                string GetHoroName(DateTime dt)
+                {
+                    int month = dt.Month;
+                    int day = dt.Day;
+                    switch (month)
+                    {
+                        case 1:
+                            if (day <= 19)
+                                return "Capricorn";
+                            else
+                                return "Aquarius";
+
+                        case 2:
+                            if (day <= 18)
+                                return "Aquarius";
+                            else
+                                return "Pisces";
+                        case 3:
+                            if (day <= 20)
+                                return "Pisces";
+                            else
+                                return "Aries";
+                        case 4:
+                            if (day <= 19)
+                                return "Aries";
+                            else
+                                return "Taurus";
+                        case 5:
+                            if (day <= 20)
+                                return "Taurus";
+                            else
+                                return "Gemini";
+                        case 6:
+                            if (day <= 20)
+                                return "Gemini";
+                            else
+                                return "Cancer";
+                        case 7:
+                            if (day <= 22)
+                                return "Cancer";
+                            else
+                                return "Leo";
+                        case 8:
+                            if (day <= 22)
+                                return "Leo";
+                            else
+                                return "Virgo";
+                        case 9:
+                            if (day <= 22)
+                                return "Virgo";
+                            else
+                                return "Libra";
+                        case 10:
+                            if (day <= 22)
+                                return "Libra";
+                            else
+                                return "Scorpio";
+                        case 11:
+                            if (day <= 21)
+                                return "Scorpio";
+                            else
+                                return "Sagittarius";
+                        case 12:
+                            if (day <= 21)
+                                return "Sagittarius";
+                            else
+                                return "Capricorn";
+                    }
+                    return "";
+                }
+                Console.WriteLine("zodie: " + GetHoroName(dt));
+            }
             return Task.FromResult(output);
         }
 
